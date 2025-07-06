77a3f1e6f665cdc3653fa0aabd5f4daf78c6b768
Donizete Queiroz <donizeteqsud@gmail.com>
2025-07-06 00:15:11 -0300

    add doctor creation form

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,8 +1,6 @@
-import { Plus } from "lucide-react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
 import {
   PageContainer,
   PageContent,
@@ -13,6 +11,8 @@ import {
   PageHeaderTitle,
 } from "@/components/ui/page-container";
 import { auth } from "@/lib/auth";
+
+import AddDoctorButton from "./_components/add-doctor-button";
 
 const DoctorsPage = async () => {
   const session = await auth.api.getSession({
@@ -34,10 +34,7 @@ const DoctorsPage = async () => {
           </PageHeaderDescription>
         </PageHeaderContent>
         <PageHeaderActions>
-          <Button>
-            <Plus />
-            Adicionar Médico
-          </Button>
+          <AddDoctorButton />
         </PageHeaderActions>
       </PageHeader>
       <PageContent>
